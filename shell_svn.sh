95fa5361a9b3c3718034ee65c0832f03848c9640
lichao <lichao0111@users.noreply.github.com>
2017-06-15 09:23:01 +0800

    Update shell_svn.sh
    简单的一个svn的linux服务器上更新代码的脚本  主要功能：代码的更新 svn update  ，代码回退到上一个版本svn up -r  ，以及单个文件的回退svn merge -r $answer $i   注意在单个文件的回退中代码会commit到svn_server，以及注意路径的配置

--- shell_svn.sh
+++ shell_svn.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #edit richard
 #2017.6.13
-#该脚本用于svn代码更新到linux服务器 注意SVN_HOME和PROJECT的对于配置
-
-SVN_HOME='/data0/web/swwsv2.258.com/'
+#该脚本用于svn代码更新/回退linux服务器代码 注意SVN_HOME和PROJECT的对于配置
+#LANG=zh_CN.GB2312
+#export LANG
+export LC_ALL=en_US.UTF-8
+#SVN_HOME='/data0/web/swwsv2.258.com/'
+SVN_HOME=/data0/web/yx.kaifa.258.com/
 PROJECT='swwsv2.258.com'
 
 
 update_version()
 {
-    echo 'project url' $SVN_HOME
+        echo 'project url' $SVN_HOME
 	svn status -u $SVN_HOME | grep -v "?"
 	svn status -u $SVN_HOME | grep -v "?"
 	echo '---------------------------'
@@ -27,24 +30,22 @@ update_version()
 	echo '---------------------------'
 	svn info $SVN_HOME                
 	echo 'please input version:'
-		read versionNo
-	if (( "$versionNo" -ne "" )):then
-
-	echo ' r u sure update? y or no?'
-		read inp
-	if [ "$inp" = "y" ]
-	#if (("$inp" == "y"))
+	read versionNo
+	if (( "$versionNo" -ne "" ))
 	then
-    echo "updateing........"
-    svn up $SVN_HOME
-	#del fields
-	echo  $SVN_HOME'Application/Runtime/common~runtime.php'
-	echo $SVN_HOME'Application/Runtime/Data/_fields/*.php'
-	#rm -rf /data0/web/swwsv2.258.com/Application/Runtime/common~runtime.php 
-	#rm -rf /data0/web/new.www.258weishi.com/Application/Runtime/Data/_fields/*.php
-	else
-		echo "lose svn up"
-	fi
+	  echo ' r u sure update? y or no?'
+	  read inp
+	  if [ "$inp" = "y" ]	    #if (("$inp" == "y"))
+	  then
+		  echo "updateing........"
+		  svn up $SVN_HOME	
+		  echo  $SVN_HOME'Application/Runtime/common~runtime.php'  #del fields
+		  echo  $SVN_HOME'Application/Runtime/Data/_fields/*.php'
+	      #rm -rf /data0/web/swwsv2.258.com/Application/Runtime/common~runtime.php 
+	      #rm -rf /data0/web/new.www.258weishi.com/Application/Runtime/Data/_fields/*.php
+	  else
+              echo "lose svn up"
+	  fi
 	fi
 
 }
@@ -60,29 +61,45 @@ rocback_all_version()
 	echo -e "\r\n"
 	echo $rocback_version 
 	echo -e "\r\n"
-	echo 'are you sure of to ' $rocback_version ' Yes or No' 
+	echo 'are you sure of to ' $rocback_version ' y or n' 
 	echo -e "\r\n"
 	read answer 
-	if [ $answer == "Yes" ]:then
-        svn up -r $rocback_version 
-        echo -e "\r\n"
-        echo 'Code is rocback is OK'
+	if [ $answer == "y" ]
+    then
+          svn up -r $rocback_version $SVN_HOME
+          echo -e "\r\n"
+          echo 'Code is rocback is OK'
 	else
-        echo ' Give up rocback '
+          echo ' Give up rocback '
 	fi
 }
 
 rocback_file_version()
 {
 
-
-
-
-
+	echo '-----show update files----------'
+	ver=`svn log $SVN_HOME --limit 2|sed '1d'|sed '$d'|awk -F '|' '{print $1}'|grep -v '-'|grep -v '^$'`	
+	ver_old=`echo $ver |awk -F 'r' '{print $2}'`  
+	file=`svn log --diff -r $ver_old $SVN_HOME |grep "Index" |awk '{print $2}'`
+	echo $file
+	echo '##choose rocback file eg: test.txt test1.txt '
+	read files
+	echo $files
+	for i in $files
+	do
+	  echo '--------------------' 
+	  echo  "正在回退"$i
+	  svn log $i |head -20
+	  echo '选择回退版本 ?eg: 22:11' 
+	  read answer
+	  echo $answer
+	  svn merge -r $answer $i
+	  svn commit -m "roll back" $i
+	done
 }
 
-echo " this shell is svn update rocback_all and rocback_fields"
-read -p  "choose update/rocback_all/rocback_fields :" some
+echo " Svn choose update/rocback_all/rocback_fiels"
+read some
 
 if [ $some == "update" ]
 then
@@ -90,10 +107,10 @@ then
 elif [ $some == "rocback_all" ]
 then  
   rocback_all_version 
-elif [ $some == "rocback_fields" ]
+elif [ $some == "rocback_fiels" ]
 then  
   rocback_file_version
 else
   exit 0
-if
+fi
 
