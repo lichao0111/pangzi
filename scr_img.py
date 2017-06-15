600bde2911f58fd6c7a39128f05128ebafa22d5d
lichao <lichao0111@users.noreply.github.com>
2017-06-15 23:06:34 +0800

    Create scr_img.py

--- scr_img.py
+++ scr_img.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python
-#edit richard 
-#2017.6.13
+# edit richard
+# 2017.6.13
 # -*- coding: UTF-8 -*-
 import os
 import commands
 import urllib
 import urllib2
 import json
-import requests
+#import requests
 
 
 def scr_im():
     post_url = 'http://api.mozhan.com/ScreenShots/getUrls'
     req = urllib2.Request(post_url)
     response = urllib2.urlopen(req)
-    url=json.load(response)
-    str=url['d']
-    for i in range(0,len(str)):
+    url = json.load(response)
+    str = url['d']
+    for i in range(0, len(str)):
         name = str[i]['url']
-        str1 = '/root/screenshot/phantomjs/bin/phantomjs /root/screenshot/resterize2.js '+name
+        str1 = '/root/screenshot/phantomjs/bin/phantomjs /root/screenshot/resterize2.js ' + name
         open_files = commands.getstatusoutput(str1)
-        mv_str = 'mv '+name+'.png'+' /root/screenshot/images/'
+        mv_str = 'mv ' + name + '.png' + ' /root/screenshot/images/'
         open_files = commands.getstatusoutput(mv_str)
-        id = str[i]['id'] 
+        id = str[i]['id']
         post_id(id)
-     
+
+
 def post_id(id):
     url1 = 'http://api.mozhan.com/ScreenShots/synResult'
-    data = id
-    response = urllib.urlopen(url1,data)
-    #d=requests.post(id_post,data=id)
-    print d
+    parmas = urllib.urlencode({'id': id})
+    f = urllib.urlopen(url1, parmas)
+    
 
 def main():
-#    scr_im()
-     id = 10
-     post_id(id)
+    #scr_im()
+    id = 10
+    post_id(id)
+
 
 if __name__ == "__main__":
     main()
+
